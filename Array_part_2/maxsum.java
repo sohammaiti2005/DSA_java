025ace0e25afff43a605fee43e457250cdc3234f
sohammaiti2005 <sohammaiti770@gmail.com>
2026-08-26 00:50:37 +0530

    prefixsum

---
+++ Array_part_2/maxsum.java
@@ -0,0 +1,41 @@
+public class maxsum {
+    public static void MaxSubArraySum(int arr[]){
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+        int prefix[]=new int[arr.length];
+        prefix[0]=arr[0];
+        // calculate prefix array
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+                currsum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+                
+                // tp=0;
+                // for(int k=i;k<=j;k++){
+                //     int num=arr[k];
+                    // System.out.print(num+" ");
+                    
+                }
+                
+                System.out.println();
+                // System.out.println("total : "+tp);
+                if(currsum>maxsum){
+                    maxsum=currsum;
+                }
+                
+            }
+            System.out.println("Maximum subarray sum : "+maxsum);
+        
+    }
+    public static void main(String[] args) {
+        int arr[]={1,-2,6,-1,3};
+        MaxSubArraySum(arr);
+
+    }
+    
+}
