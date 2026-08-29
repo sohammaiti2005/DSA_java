3df51669e754f71a1d320cec52bcd5ffd291a2cd
sohammaiti2005 <sohammaiti770@gmail.com>
2026-08-29 15:24:03 +0530

    sort

---
+++ Sorting Algo/selection.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class selection {
+    public static void select(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minpos] > arr[j]) {
+                    minpos = j;
+                }
+            }
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void insertion(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            // for(int j=i-1;j>=0;j--){
+            // if(arr[i]<arr[j]){
+            // }
+            // }
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        // select(arr);
+        insertion(arr);
+    }
+
+}
