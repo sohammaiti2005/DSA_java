cbee288ad035c4ea96bfa7df4d0426c1edeb9704
sohammaiti2005 <sohammaiti770@gmail.com>
2026-08-30 21:36:43 +0530

    modify

--- Sorting Algo/selection.java
+++ Sorting Algo/selection.java
@@ -38,11 +38,20 @@ public class selection {
             System.out.print(arr[i]);
         }
     }
+    public static void counting(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            
+        }
+    }
 
     public static void main(String args[]) {
         int arr[] = { 5, 4, 1, 3, 2 };
         // select(arr);
-        insertion(arr);
+        // insertion(arr);
+        Arrays.sort(arr,0,3);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
     }
 
 }
